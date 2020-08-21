64a0e0ab6873387cb9ae4004de452ba721dfc6e3
wilee <489556639@qq.com>
2020-08-21 11:15:04 +0800

    second commit
    1.reduce batch size because of limited GPU memory
    2.add path to system python path to resolve 'module cannot found error'

--- pyimagesearch/dogs_vs_cats/train_alexnet.py
+++ pyimagesearch/dogs_vs_cats/train_alexnet.py
@@ -3,6 +3,13 @@
 import matplotlib
 matplotlib.use("Agg")
 
+# resolve "no module found error“
+import os
+import sys
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+sys.path.append(os.path.split(rootPath)[0])
+
 # import the necessary packages
 from config import dogs_vs_cats_config as config
 from pyimagesearch.preprocessing.imagetoarraypreprocessor import ImageToArrayPreprocessor
@@ -15,7 +22,12 @@ from pyimagesearch.nn.conv.alexnet import AlexNet
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.optimizers import Adam
 import json
-import os
+import tensorflow as tf
+
+# solve failed to get convolution algorithm
+cfg = tf.compat.v1.ConfigProto()
+cfg.gpu_options.allow_growth = True
+session = tf.compat.v1.InteractiveSession(config=cfg)
 
 # construct the training image generator for data augmentation
 aug = ImageDataGenerator(rotation_range=20, zoom_range=0.15,
@@ -32,9 +44,9 @@ mp = MeanPreprocessor(means["R"], means["G"], means["B"])
 iap = ImageToArrayPreprocessor()
 
 # initialize the training and validation dataset generators
-trainGen = HDF5DatasetGenerator(config.TRAIN_HDF5, 128, aug=aug,
+trainGen = HDF5DatasetGenerator(config.TRAIN_HDF5, 32, aug=aug,
                                 preprocessors=[pp, mp, iap], classes=2)
-valGen = HDF5DatasetGenerator(config.VAL_HDF5, 128,
+valGen = HDF5DatasetGenerator(config.VAL_HDF5, 32,
                               preprocessors=[sp, mp, iap], classes=2)
 
 # initialize the optimizer
@@ -53,9 +65,9 @@ callbacks = [TrainingMonitor(path)]
 # train the network
 model.fit_generator(
     trainGen.generator(),
-    steps_per_epoch=trainGen.numImages // 128,
+    steps_per_epoch=trainGen.numImages // 32,
     validation_data=valGen.generator(),
-    validation_steps=valGen.numImages // 128,
+    validation_steps=valGen.numImages // 32,
     epochs=75,
     max_queue_size=128 * 2,
     callbacks=callbacks, verbose=1)
